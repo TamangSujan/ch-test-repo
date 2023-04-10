623aef5e3df2e3580ec1433ec9981936c66305d1
TamangSujan <moktansujanj5@gmail.com>
2023-04-10 16:20:24 +0545

    added: initial config-client properties

--- src/main/java/com/ch/service_a/ServiceAApplication.java
+++ src/main/java/com/ch/service_a/ServiceAApplication.java
@@ -2,10 +2,12 @@ package com.ch.service_a;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableConfigServer
 public class ServiceAApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/ch/service_a/controller/ServiceBController.java
+++ src/main/java/com/ch/service_a/controller/ServiceBController.java
@@ -2,6 +2,7 @@ package com.ch.service_a.controller;
 
 import com.ch.service_a.service.ServiceBService;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,10 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/web")
 public class ServiceBController {
-    private ServiceBService serviceBService;
+    private final ServiceBService serviceBService;
 //    @GetMapping("/services")
 //    public ResponseEntity<?> getServices(){
 //        return ResponseEntity.status(HttpStatus.OK).body(serviceBService.getAllServices());
